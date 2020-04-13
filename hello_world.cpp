ea96c4bccaaa981eb3e567f4227372803fba966c
M4skEl <58752844+M4skEl@users.noreply.github.com>
2020-04-13 20:39:24 +0300

    Перевод

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 int  main {
-  std::string name; //enter your name
+  std::string name; //Введите ваше имя
   std::cin>>name;
   std::cout<<"Hello world from "<<name;
   return 0;
